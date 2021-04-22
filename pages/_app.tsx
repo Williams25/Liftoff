50ce20a5f1d88257e4b9ead68180637fe42d154c
Williams25 <william.souza.timao@hotmail.com>
2021-04-22 18:58:48 -0300

    adicionando o contexto

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,19 @@
 import '../src/styles/global.scss'
 import { Header, Player } from '../src/components'
+import { PlayerProvider } from '../src/contexts'
 import styles from '../src/styles/app.module.scss'
 
 function MyApp({ Component, pageProps }) {
   return (
     <div className={styles.appWrapper}>
-      <main>
-        <Header />
-        <Component {...pageProps} />
-      </main>
+      <PlayerProvider>
+        <main>
+          <Header />
+          <Component {...pageProps} />
+        </main>
 
-      <Player />
+        <Player />
+      </PlayerProvider>
     </div>
   )
 }
